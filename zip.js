4dcd671b5aa4d6131e1f5925887b6df512ab4ba5
jasbirsinghrana <jasbirs@mindfiresolutions.com>
2020-06-28 17:22:20 +0530

    added zip generator module

--- zip.js
+++ zip.js
@@ -1,13 +1,34 @@
-var fs = require('fs');
-var archiver = require('archiver');
+let fs = require('fs');
+let archiver = require('archiver');
+
+let output = fs.createWriteStream('output.zip');
+
+let archive = archiver('zip');
+
+const getFiles = dir => {
+    let directory = fs.readdirSync(dir);
+
+    directory.forEach(file => {
+
+    	let filePath = dir + '/' + file;
+
+    	let stats = fs.statSync(filePath);
+
+        if (stats.isFile()) {
+
+            archive.file(filePath, { name: file });
+        } else {
+
+            getFiles(filePath);
+        }
+    });
+}
 
 
-var output = fs.createWriteStream('output.zip');
-
-var archive = archiver('zip');
-
-archive.directory('directory/', false);
+getFiles('./directory');
 
 archive.pipe(output);
 
 archive.finalize();
+
+console.log('Zip named output.zip has been created!');
